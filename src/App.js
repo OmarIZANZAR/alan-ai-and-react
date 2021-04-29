6dcfdaeb5d49836053d892a6f42a361f50e69cf4
omar izanzar <izanzar.omar@gmail.com>
2021-04-29 03:04:32 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import alanBtn from "@alan-ai/alan-sdk-web";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const alanBtnContainer = useRef();
+
+  useEffect(() => {
+    let alanBtnInstance = alanBtn({
+      key: '168bdb9dfdf4fae8b0f00d8a275b61172e956eca572e1d8b807a3e2338fdd0dc/stage',
+      rootEl: alanBtnContainer.current,
+      onConnectionStatus: async function(status) {
+        if (status === 'authorized') {
+          alanBtnInstance.playText('Hey, this is Alan');
+          await alanBtnInstance.activate();
+        }else {
+          alanBtnInstance.playText('this AI is expired');
+        }
+      },
+    })
+  }, []);
+
+  return <div className="App">
+    <header className="App-header">
+      <ul>
+        <li>Click the Alan button to talk if you don't know where to start, ask alan what can it do.</li>
+      </ul>
+    </header>
+    <div ref={alanBtnContainer}></div>
+  </div>;
 }
 
 export default App;
